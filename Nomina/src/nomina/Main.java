a5d9fada422f3af19bd771d9fc37231aae447110
AndresSuarezz <49999988+AndresSuarezz@users.noreply.github.com>
2022-04-04 10:41:43 -0500

    Add files via upload

---
+++ Nomina/src/nomina/Main.java
@@ -0,0 +1,12 @@
+package nomina;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vista ventana = new Vista();
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+        ventana.setLocationRelativeTo(null);
+        
+    }
+}
